c3092838dcc50d08a5f6c7f79c078bbb498a3eea
Rafael C Fernandes <rafael.londrina@gmail.com>
2020-08-23 12:39:35 -0300

    first commit modules

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '8b9d1f3c479e481c5929b8585baf9abf',
+    expiresIn: '1d',
+  },
+};
---
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+
+import AuthenticateUsersService from '@modules/users/services/AuthenticateUsersService';
+
+const sessionsRouter = Router();
+
+sessionsRouter.post('/', async (request, response) => {
+  const { email, password } = request.body;
+
+  const authenticateUser = new AuthenticateUsersService();
+
+  const { user, token } = await authenticateUser.execute({
+    email,
+    password,
+  });
+
+  delete user.password;
+
+  return response.json({ user, token });
+});
+
+export default sessionsRouter;
